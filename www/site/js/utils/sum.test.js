9d396cfeaf1f4ea1d2cd7d401bba869de9a337f1
Germán Aliprandi <galiprandi@gmail.com>
2021-04-12 20:07:43 -0300

    Play with Jest

--- www/site/js/utils/sum.test.js
+++ www/site/js/utils/sum.test.js
@@ -1,8 +1,16 @@
-import {sum} from './sum.js'
+import { sum } from './sum.js'
 // const sum = require('./sum.js')
 
-describe('For test suite status check', () => {
+describe('For check if Jest is working', () => {
   it('Sum 5 + 2 should by 7', () => {
     expect(sum(5, 2)).toBe(7)
+  })
+
+  it('Sum strings of numbers should by work them', () => {
+    expect(sum('5', '2')).toBe(7)
+  })
+
+  it('Sum strings should by Nan ', () => {
+    expect(sum('hi', 'bye')).toBe(NaN)
   })
 })
